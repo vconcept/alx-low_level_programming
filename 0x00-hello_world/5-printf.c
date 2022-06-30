c1ac8f6fc9fe2fd529f5bc3ce818e27bb0687153
vconcept <yungaeronaut@gmail.com>
2022-06-30 15:47:34 -0700

    More understood now

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
  * This will print
  * *using the printf function
  * Nice, isnt it
+ * main - Entry point
+ *
+ * Return - Always 0
  */
 
 int main(void)
---
+++ README.md
@@ -0,0 +1,1 @@
+NEW - Low level Programming
