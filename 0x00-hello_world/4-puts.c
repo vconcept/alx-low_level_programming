dd51da6f1dc7c1a3f7ee5e36fcc87916338b644b
vconcept <yungaeronaut@gmail.com>
2022-06-30 15:08:55 -0700

    Progress made to 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Writing a program using puts function to print
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * This will print
+ * *using the printf function
+ * Nice, isnt it
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
