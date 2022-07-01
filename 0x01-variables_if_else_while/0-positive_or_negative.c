9f1cda36f31bf720e6ee7bbbf64600af84039e6c
vconcept <yungaeronaut@gmail.com>
2022-07-01 14:51:36 -0700

    =ve or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Determines if a number is positive, or negative or zero
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) 
+	{
+		printf("%d is positive\n", n);
+	} elseif(n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else 
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
