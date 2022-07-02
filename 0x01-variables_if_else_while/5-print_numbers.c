5355d4d828f458f1ef31ec43294ea365b94fd11d
vconcept <yungaeronaut@gmail.com>
2022-07-01 17:58:45 -0700

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print decimals
+ *
+ * Return: Always 0 (Successful)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0, i <= 10, i++)
+	{
+	printf("%d", i);
+	}
+	putchar("\n");
+	return (0);
+}
+
