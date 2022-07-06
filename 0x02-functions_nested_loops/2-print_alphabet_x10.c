84fe19d408f557a6cd67d1ad235099ef725c9a53
vconcept <yungaeronaut@gmail.com>
2022-07-05 17:13:16 -0700

    alpga-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - print lowercase alphabet ten times
- *   * Return: 0
- *    */
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ * Return: 0
+ */
 
 void print_alphabet_x10(void)
 {
