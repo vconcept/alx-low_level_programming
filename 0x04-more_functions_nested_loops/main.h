03248ddec73cbb88f720e7940a1639c0c6e80dff
vconcept <yungaeronaut@gmail.com>
2022-07-07 15:02:13 -0700

    Comited

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #include<stdio.h>
 
-int _putchar(char* c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b)
+int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char);
 void print_most_numbers(void);
