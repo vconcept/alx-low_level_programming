1226d069c175a8dfc27cb09814d51078b22ee687
vconcept <yungaeronaut@gmail.com>
2022-07-07 13:04:08 -0700

    Done right

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - check the code for uppercase character
+ * @c: input chart
+ * Return: Always 0 if not upper, i if otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <=90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
