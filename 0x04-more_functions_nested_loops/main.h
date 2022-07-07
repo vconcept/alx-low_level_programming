df325737c6185c9e77a5bfeea1e45890e876e3b8
vconcept <yungaeronaut@gmail.com>
2022-07-07 14:53:37 -0700

    Lets see

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 int _putchar(char* c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int a, int b)
 void print_numbers(void);
 int _putchar(char);
 void print_most_numbers(void);
