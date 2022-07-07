e890b6355cd3c11690062475e329c9ef7610ebcc
vconcept <yungaeronaut@gmail.com>
2022-07-07 12:58:50 -0700

    is alright

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,15 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	if (c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
 	return (0);
 }
