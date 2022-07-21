c1ad75f39835c714518cd598db97f6dd6e42e6ba
vconcept <yungaeronaut@gmail.com>
2022-07-20 18:04:43 -0700

    string and then new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int prev, int root);
+int is_prime(unsigned int n, unsigned int c);
+int is_prime_number(int n);
+int _strlen_recursion(char *s);
+int comparator(char *s, int n1, int n2);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
