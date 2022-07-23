cf6090adce58d3be8b3d7f27a5f536c13d8e7497
vconcept <yungaeronaut@gmail.com>
2022-07-22 17:57:25 -0700

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - this program prints its name followed by a new line
+ *argc - params
+ * Return: Always 0 (Success)
+ */
+
+ int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
+
