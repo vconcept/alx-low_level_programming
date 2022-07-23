972851c885d89f1e51e685885d61b48e1639645c
vconcept <yungaeronaut@gmail.com>
2022-07-22 18:34:09 -0700

    recoded

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 
 /**
  *main - this program prints its name followed by a new line
- *argc - params
+ *@argc: argc params
+ *@argv: argv parameter
  * Return: Always 0 (Success)
  */
 
- int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
